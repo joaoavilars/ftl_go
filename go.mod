f64a577725871e5120b96b1cb626a8ef83c32649
joaoavilars <joao.avila.rs@gmail.com>
2024-01-22 17:50:30 -0300

    inicio de repositorio para salvar o script

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gerazip
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+const baseURL = "https://localhost"
+
+func main() {
+	// Flags
+	flag.Parse()
+
+	// Argumento não processado (arquivo CSV)
+	csvFile := flag.Arg(0)
+
+	// Abrir o arquivo CSV
+	file, err := os.Open(csvFile)
+	if err != nil {
+		fmt.Printf("Erro ao abrir arquivo CSV: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	// Criar um leitor CSV
+	reader := csv.NewReader(file)
+
+	// Desabilitar a verificação do certificado TLS
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Loop pelos registros do CSV
+	for {
+		// Ler o próximo registro do CSV
+		record, err := reader.Read()
+
+		// Verificar se atingiu o final do arquivo
+		if err == io.EOF {
+			break
+		}
+
+		// Verificar se ocorreu algum outro erro
+		if err != nil {
+			fmt.Printf("Erro ao ler registro do CSV: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Extrair as informações do registro
+		cnpj := record[0]
+		dataIni := record[1]
+		dataFim := record[2]
+
+		// Formatar a data inicial para o formato 'ddMMyyyy'
+		dataIniFormatted, err := time.Parse("02012006", dataIni)
+		if err != nil {
+			fmt.Printf("Erro ao formatar a data inicial: %v\n", err)
+			continue
+		}
+		dataIniFormattedStr := dataIniFormatted.Format("02012006")
+
+		// Formatar a data final para o formato 'ddMMyyyy'
+		dataFimFormatted, err := time.Parse("02012006", dataFim)
+		if err != nil {
+			fmt.Printf("Erro ao formatar a data final: %v\n", err)
+			continue
+		}
+		dataFimFormattedStr := dataFimFormatted.Format("02012006")
+
+		// URL do serviço web
+		url := fmt.Sprintf("%s/GenerateZipXml.ashx?dataini=%s&datafim=%s&cnpj=%s", baseURL, dataIniFormattedStr, dataFimFormattedStr, cnpj)
+
+		// Fazer a requisição HTTP
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("Erro ao fazer a requisição para CNPJ %s: %v\n", cnpj, err)
+			continue
+		}
+		defer response.Body.Close()
+
+		// Ler o conteúdo da resposta
+		body, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("Erro ao ler o conteúdo da resposta para CNPJ %s: %v\n", cnpj, err)
+			continue
+		}
+
+		fmt.Println(url)
+		fmt.Printf("CNPJ: %s - de %s a %s : %s\n", cnpj, dataIniFormattedStr, dataFimFormattedStr, body)
+		fmt.Println()
+	}
+}
